098a47070258d516d2d2ee4be524bcb02cfdc800
Winter Wang <winterw.wang@mail.utoronto.ca>
2022-10-20 21:30:19 -0400

    comment. add comment to mb2

--- mb/mb2.c
+++ mb/mb2.c
@@ -3,9 +3,9 @@
 int main ()
 {
   int a = 0, i;
-  for (i = 0; i < 1000000; i++)
+  for (i = 0; i < 1000000; i++) // Always Taken
   {
-    if (i % 4 == 0)
+    if (i % 4 == 0) // T T T NT
     {
       a += i;
     }
